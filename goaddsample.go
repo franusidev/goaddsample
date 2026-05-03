e5790f3dfd6151706907c46f1d83ca42a023b470
Francisco Nunez Sierra <franusidev@gmail.com>
2026-05-03 13:22:49 +0200

    doc: add documentation for the package

--- goaddsample.go
+++ goaddsample.go
@@ -1,5 +1,8 @@
+// A simple package with an add function	
 package goaddsample
 
+// Adds two numbers together
+// If you want to learn more about adding, read https://www.mathsisfun.com/numbers/addition.html
 func Add(a,b int) int {
 	return a+b
 }
